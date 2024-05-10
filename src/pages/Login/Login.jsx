ec523d6478b012cd1fc11c044c3ede424707fa42
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-05-10 15:13:24 +0600

    Navbar,Login and Register section has been done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,97 +1,87 @@
-import { useContext, useState } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { FaGithub, FaGoogle } from "react-icons/fa";
-import { AuthContext } from "../../providers/AuthProvider";
-
-
-
+import { useContext, useState } from "react"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { FaGithub, FaGoogle } from "react-icons/fa"
+import { AuthContext } from "../../providers/AuthProvider"
 
 const Login = () => {
-    const {signIn,signInWithGoogle, signInWithGithub} = useContext(AuthContext);
-    const [loginError, setLoginError] = useState('');
-    const [success, setSuccess] = useState('');
-    const location = useLocation();
-    const navigate = useNavigate();
-    console.log('Location in the login page', location);
+  const { signIn, signInWithGoogle, signInWithGithub } = useContext(AuthContext)
+  const [loginError, setLoginError] = useState("")
+  const [success, setSuccess] = useState("")
+  const location = useLocation()
+  const navigate = useNavigate()
+  console.log("Location in the login page", location)
 
-    const handleLogin = e =>{
-        e.preventDefault();
-        console.log(e.currentTarget);
-        const form = new FormData(e.currentTarget);
-        const email = form.get('email');
-        const password = form.get('password');
-        console.log(email, password);
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(e.currentTarget)
+    const form = new FormData(e.currentTarget)
+    const email = form.get("email")
+    const password = form.get("password")
+    console.log(email, password)
 
+    //reset error
+    setLoginError("")
+    setSuccess("")
 
-        //reset error
-        setLoginError('');
-        setSuccess('');
-
-            //Password should be 6 character
-    if(password.length < 6){
-      setLoginError('Password should be at least 6 characters');
-      return;
-    }
-    else if(!/[A-Z]/.test(password)){
-      setLoginError('Your Password should be at least one uppercase');
-      return;
-    }
-    else if(!/[a-z]/.test(password)){
-      setLoginError('Your Password should be at least one lowercase');
-      return;
+    //Password should be 6 character
+    if (password.length < 6) {
+      setLoginError("Password should be at least 6 characters")
+      return
+    } else if (!/[A-Z]/.test(password)) {
+      setLoginError("Your Password should be at least one uppercase")
+      return
+    } else if (!/[a-z]/.test(password)) {
+      setLoginError("Your Password should be at least one lowercase")
+      return
     }
 
-        signIn(email, password)
-        .then(result => {
-            console.log(result.user);
-            setSuccess('User Login Successfully');
-
-            //navigate after login
-            navigate(location?.state ? location.state : '/');
-
-
-        })
-        .catch(error => {
-            console.error(error);
-            setLoginError(error.message)
-        })
-    }
-    //FOR GOOGLE SIGN IN 
-    const handleGoogleSignIn = () => {
-      signInWithGoogle()
-      .then(result => {
+    signIn(email, password)
+      .then((result) => {
         console.log(result.user)
-        navigate(location?.state ? location.state : '/');
+        setSuccess("User Login Successfully")
+
+        //navigate after login
+        navigate(location?.state ? location.state : "/")
       })
-      .catch(error => {
+      .catch((error) => {
+        console.error(error)
+        setLoginError(error.message)
+      })
+  }
+  //FOR GOOGLE SIGN IN
+  const handleGoogleSignIn = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result.user)
+        navigate(location?.state ? location.state : "/")
+      })
+      .catch((error) => {
         console.log(error)
       })
-    }
+  }
 
-    //FOR GITHUB SIGN IN
-    const handleGithubSignIn = () => {
-      signInWithGithub()
-      .then(result => {
-        console.log(result.user);
-        navigate(location?.state ? location.state : '/');
+  //FOR GITHUB SIGN IN
+  const handleGithubSignIn = () => {
+    signInWithGithub()
+      .then((result) => {
+        console.log(result.user)
+        navigate(location?.state ? location.state : "/")
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error)
       })
-    }
+  }
 
-
-
-
-
-    return (
-        <div data-aos="fade-down" data-aos-duration="2000">
-      {/* <Helmet>
-        <title>Login</title>
-      </Helmet> */}
+  return (
+    <div data-aos="fade-down" data-aos-duration="2000">
       <div>
-        <h2 className="text-3xl text-center font-bold text-blue-600">Login Now</h2>
-        <form onSubmit={handleLogin}  className="card-body md:w-3/4 lg:w-1/2 mx-auto">
+        <h2 className="text-3xl text-center font-bold text-blue-600">
+          Login Now
+        </h2>
+        <form
+          onSubmit={handleLogin}
+          className="card-body md:w-3/4 lg:w-1/2 mx-auto"
+        >
           <div className="form-control">
             <label className="label">
               <span className="label-text">Email</span>
@@ -120,35 +110,55 @@ const Login = () => {
                 Forgot password?
               </a>
             </label>
-            {
-            loginError && <p className="text-red-700 text-center">{loginError}</p>
-           } 
-           {
-            success && alert('User Login Successfully')
-           }
+            {loginError && (
+              <p className="text-red-700 text-center">{loginError}</p>
+            )}
+            {success && alert("User Login Successfully")}
           </div>
           <div className="form-control mt-6">
             <button className="btn btn-primary">Login</button>
           </div>
         </form>
 
-          {/* {/*Login With Google And Github  */}
+        {/* {/*Login With Google And Github  */}
         <div className="lg:ml-[360px] p-4 space-y-3 ml-10">
-          <h2 className="text-3xl lg:ml-12">Login With</h2>
-             <button data-aos="fade-up" data-aos-duration="2000" onClick={handleGoogleSignIn} className="btn btn-outline w-1/5">
-             <FaGoogle />
-             Google
-             </button>
-             <button data-aos="fade-up" data-aos-duration="2000" onClick={handleGithubSignIn} className="btn btn-outline w-1/5 ml-2">
-             <FaGithub />
-             Github
-             </button>
+          <h2 className="text-3xl lg:ml-[170px]">Login With</h2>
+          <div className="lg:ml-24">
+            <button
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              onClick={handleGoogleSignIn}
+              className="btn btn-outline w-1/5"
+            >
+              <FaGoogle />
+              Google
+            </button>
+            <button
+              data-aos="fade-up"
+              data-aos-duration="2000"
+              onClick={handleGithubSignIn}
+              className="btn btn-outline w-1/5 ml-2"
+            >
+              <FaGithub />
+              Github
+            </button>
+          </div>
         </div>
-
-        <p data-aos="fade-up" data-aos-duration="2000" className="text-center mb-5">Do not have an account? Please <Link className="text-blue-600 font-bold" to='/register'>Register</Link></p>
+        <div className="mr-4">
+          <p
+            data-aos="fade-up"
+            data-aos-duration="2000"
+            className="text-center mb-5"
+          >
+            Do not have an account? Please{" "}
+            <Link className="text-blue-600 font-bold" to="/register">
+              Register
+            </Link>
+          </p>
+        </div>
       </div>
-     </div>
-    );
-};
+    </div>
+  )
+}
 
-export default Login;
+export default Login
